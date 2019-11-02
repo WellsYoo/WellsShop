c4bc551dd6d0bb22c1f464723c0547d07b2bd051
ywh <ywh@meitu.com>
2019-11-02 22:04:37 +0800

    【Feature】轮播图

--- wells_shop/lib/pages/home_page.dart
+++ wells_shop/lib/pages/home_page.dart
@@ -1,13 +1,69 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据';
+
+  @override
+  void initState() {
+    getHomePageContent().then((value) {
+      setState(() {
+        homePageContent = value.toString();
+      });
+    });
+
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('百姓生活+')),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot){
+          if(snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDateList: swiper)
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中...'),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+  SwiperDiy({Key key, this.swiperDateList}):super(key:key);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-        body: Center(
-          child: Text('首页'),
-        ),
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index){
+          return Image.network("${swiperDateList[index]['image']}",fit: BoxFit.fill);
+        },
+        itemCount: 3,
+        pagination:SwiperPagination(),
+        autoplay: true,
+
       ),
     );
   }
